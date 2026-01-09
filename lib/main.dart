a6ffed91953e788a3ce61f672eb7b945f459a36c
deepeshy285 <deepeshy2895@gmail.com>
2026-01-09 22:05:35 +0545

    Navigation and Routing Using Navigation.push and Navigation.pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,40 @@
 import 'package:flutter/material.dart';
+import './SecondPage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Counter(),
-    );
+    return MaterialApp(home: FirstPage());
   }
 }
 
-class Counter extends StatefulWidget {
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    counter = 0;
-  }
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Counter App"),
+      appBar: AppBar(
+        title: Text("First page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  print("Button is pressed");
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SecondPage()));
+                },
+                child: Text("Click to Second Page")),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "$counter",
-                style: TextStyle(fontSize: 50),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;
-                    });
-                  },
-                  child: Icon(Icons.add)),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter--;
-                    });
-                  },
-                  child: Icon(Icons.remove))
-            ],
-          ),
-        ));
-  }
-  @override
-  void dispose() {
-    super.dispose();
+      ),
+    );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:testapp/counterController.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   final CounterController controller = Get.put(CounterController());
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       home: Scaffold(
-//           appBar: AppBar(
-//             title: Text("GetX State Management"),
-//           ),
-//           body: Center(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Obx(() => Text(
-//                       "${controller.count}",
-//                       style: TextStyle(fontSize: 50),
-//                     )),
-//                 const SizedBox(height: 20),
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     IconButton(
-//                       onPressed: controller.increase,
-//                       icon: Icon(Icons.add),
-//                     ),
-//                     SizedBox(
-//                       height: 20,
-//                     ),
-//                     IconButton(
-//                       onPressed: controller.decrease,
-//                       icon: Icon(Icons.remove),
-//                     )
-//                   ],
-//                 )
-//               ],
-//             ),
-//           )),
-//     );
-//   }
-// }
